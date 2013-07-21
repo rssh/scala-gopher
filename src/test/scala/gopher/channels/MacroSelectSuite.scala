806db8712f93b0e79ceabc617b819eaa0f53914c
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2013-07-21 09:54:45 +0300

    added foreach variant without shutdown

--- src/test/scala/gopher/channels/MacroSelectSuite.scala
+++ src/test/scala/gopher/channels/MacroSelectSuite.scala
@@ -41,8 +41,8 @@ class MacroSelectSuite extends FunSuite
      
    }
 
-   /*
-   test("select emulation without type in case")  {
+   
+   test("select emulation with shortcut form of go")  {
 
      val channel = makeChannel[Int](100)
 
@@ -53,24 +53,31 @@ class MacroSelectSuite extends FunSuite
      
      var sum = 0;
      val consumer = go {
-       for(s <- select) {
-          s match {
-             case `channel` ~> i =>  // withput type: now looks impossible
+       select foreach {
+          {
+             case `channel` ~> (i:Int) =>  // withput type: now looks impossible
                      //System.err.println("received:"+i)
                      sum = sum + i
-                     if (i==1000)  s.shutdown()
+                    // if (i==1000)  s.shutdown()
           }
        }
        sum
      }
 
-     Await.ready(consumer, 5.second)
-
+     try {
+       // since we can't say shutdowns, consumer will be in loop forever
+       val r = Await.result(consumer, 1.second)
+       System.out.println("r="+r);
+     }catch{
+       case ex: TimeoutException => 
+         //do nothing
+         info("timeout")
+     }
+     
      val xsum = (1 to 1000).sum
      assert(xsum == sum)
 
    }
-   * 
-   */
+   
    
 }
