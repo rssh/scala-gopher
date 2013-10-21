90a02068e332fb48f65000e6810549ae06bb4d68
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2013-10-21 23:07:28 +0300

    simplicify to get unsugared async interface

--- src/main/scala/gopher/channels/ChannelAction.scala
+++ src/main/scala/gopher/channels/ChannelAction.scala
@@ -1,30 +1,32 @@
 package gopher.channels
 
+import scala.concurrent._
+
 trait ChannelAction
 
 
 trait ReadAction[-A] extends ChannelAction
-   with (ReadActionInput[A] => ReadActionOutput)
+   with (ReadActionInput[A] => Future[ReadActionOutput])
   
-case class ReadActionInput[+A]( tie: TieReadJoin[A] , channel: InputChannel[A], value:A)
+case class ReadActionInput[+A]( tie: TieReadJoin[A] , channel: InputChannel[A], value: A)
 case class ReadActionOutput(readed: Boolean, continue: Boolean)
   
 
 trait WriteAction[A] extends ChannelAction
-  with (WriteActionInput[A] => WriteActionOutput[A] )
+  with (WriteActionInput[A] => Future[WriteActionOutput[A]] )
   
 case class WriteActionInput[A](tie: TieWriteJoin[A], channel: OutputChannel[A] )
 case class WriteActionOutput[A](writed: Option[A], continue: Boolean)
 
 trait IdleAction extends ChannelAction
-    with (TieJoin => Boolean)
+    with (TieJoin => Future[Boolean])
 
   
 object IdleAction 
 {
 
    val doNothing: IdleAction = new IdleAction{
-     def apply(tie: TieJoin): Boolean = true
+     def apply(tie: TieJoin): Future[Boolean] = Promise.successful(true).future
    }
 
 }
