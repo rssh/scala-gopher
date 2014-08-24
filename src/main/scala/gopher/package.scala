bbb88848e4c88938859b32554f2a31835dadb99a
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2014-08-24 22:22:38 +0300

    added magnets for onRead entries.

---
+++ src/main/scala/gopher/package.scala
@@ -0,0 +1,33 @@
+package object gopher {
+
+import scala.concurrent._
+import gopher.channels._
+
+ implicit def toAsyncFullReadSelectorArgument[A,B](
+                   f: (A, ContRead[A,B]) => Option[Future[Continuated[B]]]
+              ): ReadSelectorArgument[A,B] = AsyncFullReadSelectorArgument(f)  
+
+ implicit def toAsyncNoOptionReadSelectorArgument[A,B](
+                   f: (A, ContRead[A,B]) => Future[Continuated[B]]
+               ): ReadSelectorArgument[A,B] = AsyncNoOptionReadSelectorArgument(f)
+
+
+ implicit def toSyncReadSelectorArgument[A,B](
+                   f: (A, ContRead[A,B]) => Continuated[B]
+               ):ReadSelectorArgument[A,B] = SyncReadSelectorArgument(f)
+
+
+ implicit def toAsyncFullWriteSelectorArgument[A,B](
+                   f: ContWrite[A,B] => Option[(A,Future[Continuated[B]])]
+              ):WriteSelectorArgument[A,B] = AsyncFullWriteSelectorArgument(f)
+
+ implicit def toAsyncNoOptWriteSelectorArgument[A,B](
+                   f: ContWrite[A,B] => (A,Future[Continuated[B]])
+              ):WriteSelectorArgument[A,B] = AsyncNoOptWriteSelectorArgument(f)
+
+ implicit def toSyncWriteSelectorArgument[A,B](
+                   f: ContWrite[A,B] => (A,Continuated[B])
+              ): WriteSelectorArgument[A,B] = SyncWriteSelectorArgument(f)
+
+
+}
