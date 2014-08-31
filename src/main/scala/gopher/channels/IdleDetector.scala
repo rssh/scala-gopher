711e42b76fa617bfb67f9f521cbcf80b30adde52
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2014-08-31 18:32:16 +0300

    added idle detector

---
+++ src/main/scala/gopher/channels/IdleDetector.scala
@@ -0,0 +1,76 @@
+package gopher.channels
+
+import gopher._
+import scala.concurrent._
+import scala.concurrent.duration._
+import java.util.concurrent.atomic.AtomicLong
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.{LinkedList => JLinkedList}
+
+class IdleDetector(api: GopherAPI)
+{
+
+   def put(s: Selector[_]): Unit =
+   {
+     selectors add SelectorRecord(0L,s)
+     if (!idleDetectorActive) {
+          idleDetectorActive=true
+          // TODO: get from config.
+          api.actorSystem.scheduler.schedule(
+             100 milliseconds,
+             500 milliseconds){
+                       () => detect
+             }(api.executionContext)
+     }
+   }
+
+   def remove(s: Selector[_]):Unit =
+   {
+     val it = selectors.iterator();
+     var found = false
+     while(it.hasNext() && !found) {
+       val sr = it.next()
+       if (sr.selector eq s) {
+           selectors.remove(sr)
+       }
+     }
+   }
+
+   private[this] def detect: Unit =
+   {
+    var q=false;
+    var nonIdles = new JLinkedList[SelectorRecord]()
+    while(!q) {
+      val sr = selectors.poll()
+      if (sr==null) {
+        q=true
+      } else {
+        val s = sr.selector
+        if (!s.isCompleted) {
+          if (s.isLocked) {
+           nonIdles add sr
+          } else {
+           val nOps = s.nOperations.get
+           if (nOps != sr.prevNOperations) {
+              nonIdles add SelectorRecord(nOps,s)
+           } else {
+              s.startIdles
+           }
+          }
+        }
+      }
+    }
+    selectors.addAll(nonIdles)
+   }
+
+
+
+   case class SelectorRecord(
+     prevNOperations: Long,
+     selector: Selector[_]
+   )
+
+
+   val selectors = new ConcurrentLinkedQueue[SelectorRecord]()
+   @volatile var idleDetectorActive = false;
+}
