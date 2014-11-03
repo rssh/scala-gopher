9d0d4a1e34d728dc8b04dc96dac1ffc88e52e133
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2014-11-03 06:37:52 +0200

    snap to cleanuo

--- src/main/scala/gopher/channels/SelectorBuilder.scala
+++ src/main/scala/gopher/channels/SelectorBuilder.scala
@@ -12,42 +12,5 @@ trait SelectorBuilder[A]
 
    def api: GopherAPI
 
-   private[gopher] var selector=new Selector[A](api)
-
-}
-
-object SelectorBuilder
-{
-
-
-}
-
-/**
- * Builder for 'forever' selector. Can be obtained as `gopherApi.select.forever`.
- **/
-trait ForeverSelectorBuilder extends SelectorBuilder[Unit]
-{
-
-         
-
-}
-
-
-class SelectFactory(api: GopherAPI)
-{
- 
-  selectFactory =>
-
-  trait SelectFactoryApi
-  {
-    def api = selectFactory.api
-  }
-
-  /**
-   * forever builder. 
-   *@see ForeverSelectorBuilder
-   */
-  def forever: ForeverSelectorBuilder = new ForeverSelectorBuilder with SelectFactoryApi {}
-
 }
 
