5f0f26590749d5e2a47dd714a71c7276432725bf
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2017-06-03 16:55:00 +0300

    more tests transformed to async form

--- src/test/scala/gopher/channels/InputOpsSuite.scala
+++ src/test/scala/gopher/channels/InputOpsSuite.scala
@@ -53,9 +53,150 @@ class InputOpsAsyncSuite extends AsyncFunSuite  {
         } yield l
     }
 
+    test("zip operation from two finite channels") {
+        val ch1 = Input.asInput(List(1,2),gopherApi)
+        val ch2 = Input.asInput(List(1,2,3,4,5,6),gopherApi)
+        val zipped = ch1 zip ch2
+        for{
+            r1 <- zipped.aread
+            a1 = assert(r1 == (1, 1))
+            r2 <- zipped.aread
+            a2 = assert(r2 == (2,2))
+            r3 <- recoverToSucceededIf[ChannelClosedException]{ zipped.aread }
+        } yield r3
+    }
+
+    test("take from zip") {
+        val ch1 = Input.asInput(List(1,2,3,4,5),gopherApi)
+        val ch2 = Input.asInput(List(1,2,3,4,5,6),gopherApi)
+        val zipped = ch1 zip ch2
+        for {ar <- zipped.atake(5)
+             _ <- assert(ar(0) == (1, 1))
+             l <- assert(ar(4) == (5, 5))
+        } yield l
+    }
+
+    test("taking from iterator-input") {
+        val ch1 = Input.asInput(List(1,2,3,4,5),gopherApi)
+        for( ar <- ch1.atake(5) ) yield assert(ar(4)==5)
+    }
+
+    test("zip with self will no dup channels, but generate (odd, even) pairs. It's a feature, not a bug") {
+        val ch = gopherApi.makeChannel[Int]()
+        val zipped = ch zip ch
+        ch.awriteAll(List(1,2,3,4,5,6,7,8))
+        for{ r1 <- zipped.aread
+             a1 = assert( Set((1,2),(2,1)) contains r1  )
+             r2 <- zipped.aread
+             a2 = assert( Set((3,4),(4,3)) contains r2  )
+             r3 <- zipped.aread
+             a3 = assert( Set((5,6),(6,5)) contains r3  )
+        } yield a3
+    }
+
+    test("reading from Q1|Q2") {
+
+        val ch1 = gopherApi.makeChannel[Int]()
+        val ch2 = gopherApi.makeChannel[Int]()
+
+        val ar1 = (ch1 | ch2).aread
+        ch1.awrite(1)
+        for{
+            r1 <- ar1
+            a1 <- assert( r1==1 )
+            ar2 = (ch1 | ch2).aread
+            _ = ch2.awrite(2)
+            r2 <- ar2
+            a2 <- assert( r2==2 )
+        } yield a1
+
+    }
+
+    test("simultanuos reading from Q1|Q2") {
+
+        val ch1 = gopherApi.makeChannel[Int]()
+        val ch2 = gopherApi.makeChannel[Int]()
+
+        val ar1 = (ch1 | ch2).aread
+        val ar2 = (ch1 | ch2).aread
+
+        ch1.awrite(1)
+        ch2.awrite(2)
+
+        for {r1 <- ar1
+             r2 <- ar2
+             _ = if (r1 == 1) {
+                 assert(r2 == 2)
+             } else {
+                 assert(r2 == 1)
+             }
+             r3 <- recoverToSucceededIf[TimeoutException] {
+                timeouted( (ch1 | ch2).aread, 300 milliseconds)
+             }
+        } yield r3
+
+    }
+
+
+    test("reflexive or  Q|Q") {
+        val ch = gopherApi.makeChannel[Int]()
+        val aw1 = ch.awrite(1)
+        val ar1 = (ch | ch).aread
+        for {r1 <- ar1
+             _ = assert(r1 == 1)
+             ar2 = (ch | ch).aread
+             r2_1 <- recoverToSucceededIf[TimeoutException] {
+                 timeouted(ar2, 300 milliseconds)
+             }
+             _ = ch.awrite(3)
+             r2 <- ar2
+             a = assert(r2 == 3)
+        } yield a
+    }
+
+    test("two items read from Q1|Q2") {
+        val ch1 = gopherApi.makeChannel[Int]()
+        val ch2 = gopherApi.makeChannel[Int]()
+        val aw1 = ch1.awrite(1)
+        val aw2 = ch2.awrite(2)
+        val chOr = (ch1 | ch2)
+        val ar1 = chOr.aread
+        val ar2 = chOr.aread
+        for {r1 <- ar1
+             r2 <- ar2
+        } yield assert( ((r1,r2)==(1,2)) ||((r1,r2)==(2,1)) )
+    }
+
+    test("atake read from Q1|Q2") {
+        val ch1 = gopherApi.makeChannel[Int]()
+        val ch2 = gopherApi.makeChannel[Int]()
+
+        val aw1 = ch1.awriteAll(1 to 2)
+        val aw2 = ch2.awriteAll(1 to 2)
+        val at = (ch1 | ch2).atake(4)
+        for( r <- at) yield assert(r.nonEmpty)
+    }
+
+    test("awrite/take ") {
+        val ch = gopherApi.makeChannel[Int]()
+        val aw = ch.awriteAll(1 to 100)
+        val at = ch.atake(100)
+        for (r <- at) yield assert(r.size == 100)
+    }
+
+
 
     def gopherApi = CommonTestObjects.gopherApi
 
+    def timeouted[T](f:Future[T],timeout:FiniteDuration):Future[T] =
+    {
+        val p = Promise[T]()
+        p.completeWith(f)
+        gopherApi.actorSystem.scheduler.scheduleOnce(timeout){
+            p.failure(new TimeoutException)
+        }
+        p.future
+    }
 
 }
 
@@ -63,141 +204,6 @@ class InputOpsAsyncSuite extends AsyncFunSuite  {
 class InputOpsSyncSuite extends FunSuite with Waiters {
 
   import scala.concurrent.ExecutionContext.Implicits.global
-
-        
-  test("zip operation from two finite channels") {
-      val ch1 = Input.asInput(List(1,2),gopherApi)
-      val ch2 = Input.asInput(List(1,2,3,4,5,6),gopherApi)
-      val zipped = ch1 zip ch2
-      val r1 = Await.result(zipped.aread, 10 seconds)
-      assert(r1 == (1, 1))
-      val r2 = Await.result(zipped.aread, 10 seconds)
-      assert(r2 == (2, 2))
-      intercept[ChannelClosedException] {
-        val r3 = Await.result(zipped.aread, 10 seconds)
-      }
-  }
-
-  test("take from zip") {
-      val ch1 = Input.asInput(List(1,2,3,4,5),gopherApi)
-      val ch2 = Input.asInput(List(1,2,3,4,5,6),gopherApi)
-      val zipped = ch1 zip ch2
-      val at = zipped.atake(5)
-      var ar = Await.result(at, 10 seconds)
-      assert(ar(0)==(1,1))
-      assert(ar(4)==(5,5))
-  }
-
-  test("taking from iterator-input") {
-      val ch1 = Input.asInput(List(1,2,3,4,5),gopherApi)
-      val at = ch1.atake(5)
-      var ar = Await.result(at, 10 seconds)
-      assert(ar(4)==5)
-  }
-
-  test("zip with self will no dup channels, but generate (odd, even) pairs. It's a feature, not a bug") {
-      val ch = gopherApi.makeChannel[Int]()
-      val zipped = ch zip ch
-      val ar1 = zipped.aread
-      ch.awriteAll(List(1,2,3,4,5,6,7,8))
-      assert( Set((1,2),(2,1)) contains Await.result(ar1, 10 seconds)  )
-      val ar2 = zipped.aread
-      assert( Set((3,4),(4,3)) contains Await.result(ar2, 10 seconds)  )
-      val ar3 = zipped.aread
-      assert( Set((5,6),(6,5)) contains Await.result(ar3, 10 seconds) )
-  }
-
-  test("reading from Q1|Q2") {
-
-      val ch1 = gopherApi.makeChannel[Int]()
-      val ch2 = gopherApi.makeChannel[Int]()
-
-      val ar1 = (ch1 | ch2).aread
-      ch1.awrite(1)
-
-      val r1 = Await.result(ar1, 10 seconds)
-      assert(r1==1)
-      
-      val ar2 = (ch1 | ch2).aread
-      ch2.awrite(2)
-      val r2 = Await.result(ar2, 10 seconds)
-      assert(r2==2)
-
-  }
-
-  test("simultanuos reading from Q1|Q2") {
-
-      val w = new Waiter()
-
-      val ch1 = gopherApi.makeChannel[Int]()
-      val ch2 = gopherApi.makeChannel[Int]()
-
-      val ar1 = (ch1 | ch2).aread
-      val ar2 = (ch1 | ch2).aread
-
-      ch1.awrite(1)
-      ch2.awrite(2)
-
-      val r1 = Await.result(ar1, 10 seconds)
-      val r2 = Await.result(ar2, 10 seconds)
-
-      if (r1 == 1) {
-        assert(r2 == 2)
-      } else {
-        assert(r2 == 1)
-      }
-
-      val ar3 = (ch1 | ch2).aread
-      intercept[TimeoutException] {
-         val r3 = Await.result(ar3, 300 milliseconds)
-      }
-
-  }
-
-  test("reflexive or  Q|Q") {
-      val ch = gopherApi.makeChannel[Int]()
-      val aw1 = ch.awrite(1)
-      val ar1 = (ch | ch).aread
-      val r1 = Await.result(ar1, 10 seconds)
-      assert(r1==1)
-      val ar2 = (ch | ch).aread
-      intercept[TimeoutException] {
-         val r2_1 = Await.result(ar2, 300 milliseconds)
-      }
-      val aw2 = ch.awrite(3)
-      val r2 = Await.result(ar2, 10 seconds)
-      assert(r2==3)
-  }
-
-  test("two items read from Q1|Q2") {
-      val ch1 = gopherApi.makeChannel[Int]()
-      val ch2 = gopherApi.makeChannel[Int]()
-      val aw1 = ch1.awrite(1)
-      val aw2 = ch2.awrite(2)
-      val chOr = (ch1 | ch2)
-      val ar1 = chOr.aread
-      val ar2 = chOr.aread
-      val r1 = Await.result(ar1, 10 seconds)
-      val r2 = Await.result(ar2, 10 seconds)
-      assert( ((r1,r2)==(1,2)) ||((r1,r2)==(2,1)) )
-  }
-
-  test("atake read from Q1|Q2") {
-      val ch1 = gopherApi.makeChannel[Int]()
-      val ch2 = gopherApi.makeChannel[Int]()
-
-      val aw1 = ch1.awriteAll(1 to 2)
-      val aw2 = ch2.awriteAll(1 to 2)
-      val at = (ch1 | ch2).atake(4)
-      val r = Await.result(at, 10 seconds)
-  }
-
-  test("awrite/take ") {
-      val ch = gopherApi.makeChannel[Int]()
-      val aw = ch.awriteAll(1 to 100)
-      val at = ch.atake(100)
-      val r = Await.result(at, 10 seconds)
-  }
 
   test("Input foreach on closed stream must do nothing ") {
       val ch = gopherApi.makeChannel[Int]()
