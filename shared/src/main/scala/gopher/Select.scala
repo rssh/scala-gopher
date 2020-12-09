7a50e85d4a0b2de2f162783ea7b3afd19565d2c4
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-12-09 11:52:03 +0200

    SelectMacro in progress

--- shared/src/main/scala/gopher/Select.scala
+++ shared/src/main/scala/gopher/Select.scala
@@ -12,9 +12,9 @@ class Select[F[_]:CpsSchedulingMonad](api: Gopher[F]):
 object Select:
 
   sealed trait SelectorCaseExpr
-  case class ReadExpression[F[_],A, S](ch: Expr[ReadChannel[F,A]], f: Expr[(FlowTermination, A) => F[S]])
-  case class WriteExpression[F[_],A, S](ch: Expr[WriteChannel[F,A]], a: Expr[A], f: Expr[FlowTermination => F[S]])
-  case class DefaultExpression[F[_],S](ch: Expr[FlowTermination => F[S]])
+  case class ReadExpression[F[_], A, S](ch: Expr[ReadChannel[F,A]], f: Expr[A => S]) extends SelectorCaseExpr
+  case class WriteExpression[F[_], A, S](ch: Expr[WriteChannel[F,A]], a: Expr[A], f: Expr[() => F[S]]) extends SelectorCaseExpr
+  case class DefaultExpression[S](ch: Expr[ () => S ]) extends SelectorCaseExpr
   
 
   def onceImpl[F[_]:Type, A:Type](pf: Expr[PartialFunction[Any,A]])(using Quotes): Expr[A] =
@@ -29,35 +29,69 @@ object Select:
       case Inlined(_,List(),body) => 
         onceImplTree[F,A](body)
       case Match(scrutinee,cases) =>
-        val caseExprs = cases map(x => parseCaseDef(x))
+        val caseExprs = cases map(x => parseCaseDef[F,A](x))
     ???
 
-  def parseCaseDef(using Quotes)(caseDef: quotes.reflect.CaseDef): SelectorCaseExpr =
+  def parseCaseDef[F[_]:Type,S:Type](using Quotes)(caseDef: quotes.reflect.CaseDef): SelectorCaseExpr =
     import quotes.reflect._
     caseDef.pattern match 
       case Inlined(_,List(),body) => 
             parseCaseDef(CaseDef(body, caseDef.guard, caseDef.rhs))
-      case Typed(expr, TypeSelect(ch,"read")) =>
+      case Typed(expr, tp@TypeSelect(ch,"read")) =>
         if (caseDef.guard.isDefined) then
           report.error("guard in read should be empty", caseDef.asExpr)
         println(s"caseDef.rhs.tpe=${caseDef.rhs.tpe}" )
-      case Bind(v, Typed(expr, TypeSelect(ch,"read"))) =>
-          val paramName = v match
-            case Ident(x) => x
-            case _ =>
-               report.error("expected identifier in read pattern", v.asExpr)
-               "x"
-          val mt = MethodType(List(paramName))(_ => List(v.tpe), _ => caseDef.rhs.tpe)
+      case b@Bind(v, tp@Typed(expr, TypeSelect(ch,"read"))) =>
+          val mt = MethodType(List(v))(_ => List(tp.tpe), _ => caseDef.rhs.tpe)
           val readFun = Lambda(Symbol.spliceOwner,mt,
-                               (owner, args) => substIdent(body,v,args.head).changeOwner(owner))
-          ReadExpression(ch.asExpr,readFun)
-      case Bind(v, Typed(expr, TypeSelect(ch,"write"))) =>
-          println(s"!!! write discovered" )
+                               (owner, args) => substIdent(caseDef.rhs,b.symbol,args.head.asInstanceOf[Term], owner).changeOwner(owner))
+          if (ch.tpe <:< TypeRepr.of[ReadChannel[F,?]]) 
+            tp.tpe.asType match
+              case '[a] => ReadExpression(ch.asExprOf[ReadChannel[F,a]],readFun.asExprOf[a=>S])
+              case _ => 
+                report.error("can't determinate read type", caseDef.pattern.asExpr)
+                throw new RuntimeException("Can't determinae read type")
+          else
+            report.error("read pattern is not a read channel", ch.asExpr)
+            throw new RuntimeException("Incorrect select caseDef")
+      case Bind(v, tp@Typed(expr, TypeSelect(ch,"write"))) =>
+          val mt = MethodType(List())(_ => List(), _ => caseDef.rhs.tpe)
+          val writeFun = Lambda(Symbol.spliceOwner,mt, (owner,args) => caseDef.rhs.changeOwner(owner))    
+          val e = caseDef.guard match
+            case Some(condition) =>
+              condition match
+                case Apply(quotes.reflect.Select(Ident(v1),method),List(expr)) =>
+                  if (v1 != v) {
+                     report.error(s"write name mismatch ${v1}, expected ${v}", condition.asExpr)
+                     throw new RuntimeException("macro failed")
+                  }
+                  expr
+                case _ =>  
+                  report.error(s"Condition is not in form x==expr,${condition} ",condition.asExpr)
+                  throw new RuntimeException("condition is not in writing form")
+          if (ch.tpe <:< TypeRepr.of[WriteChannel[F,?]]) then
+            tp.tpe.asType match
+              case '[a] => 
+                WriteExpression(ch.asExprOf[WriteChannel[F,a]],e.asExprOf[a], writeFun.asExprOf[Unit=>S]) 
+              case _ =>
+                report.error("Can't determinate ") 
       case _ =>
         println(s"unparsed caseDef pattern: ${caseDef.pattern}" )
     ???
 
     
-    def substIdent(using Quotes)(term: Term, fromTerm: Term, toTerm: Term):Term = ???
+  def substIdent(using Quotes)(term: quotes.reflect.Term, 
+                                 fromSym: quotes.reflect.Symbol, 
+                                 toTerm: quotes.reflect.Term,
+                                 owner: quotes.reflect.Symbol): quotes.reflect.Term = 
+      import quotes.reflect._
+      val argTransformer = new TreeMap() {
+        override def transformTerm(tree: Term)(owner: Symbol):Term =
+          tree match
+            case Ident(name) if tree.symbol == fromSym => toTerm
+            case _ => super.transformTerm(tree)(owner)
+      }
+      argTransformer.transformTerm(term)(owner)
+
 
     
