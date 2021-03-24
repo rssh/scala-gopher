0aea6fa936603c59913a6dc0eaadbc8bf13d5613
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-03-24 18:29:30 +0200

    ported unbuffer3d-select-suite

--- shared/src/test/scala/gopher/channels/UnbufferedSelectSuite.scala
+++ shared/src/test/scala/gopher/channels/UnbufferedSelectSuite.scala
@@ -1,26 +1,24 @@
 package gopher.channels
 
+import cps._
 import gopher._
-import gopher.channels._
-import gopher.tags._
+import munit._
 
-
-import scala.language._
+import scala.language.postfixOps
 import scala.concurrent._
 import scala.concurrent.duration._
 
-import org.scalatest._
-import org.scalatest.concurrent._
 
-class UnbufferedSelectSuite extends FunSuite with Waiters
+class UnbufferedSelectSuite extends FunSuite 
 {
 
+   import cps.monads.FutureAsyncMonad
    import scala.concurrent.ExecutionContext.Implicits.global
+
+   given Gopher[Future] = SharedGopherAPI.apply[Future]()
 
   
    test("write without read must block ")  {
-     import gopherApi._
-     for(i <- 0 until 100) {
        val channel1 = makeChannel[Int](0)
        val w1 = channel1.awrite(1)
 
@@ -28,44 +26,46 @@ class UnbufferedSelectSuite extends FunSuite with Waiters
 
        val r1 = channel1.aread
 
-       Await.ready(w1, 10 seconds)
-       Await.ready(r1, 10 seconds)
-
-       assert(w1.isCompleted)
-       assert(r1.isCompleted)
-
-       val rd = Await.result(r1, 10 seconds)
-       assert(rd==1)
-     }
+       async {
+          await(w1)
+          await(r1)
+          val rd = await(r1)
+          assert(rd==1)
+       }
 
    }
 
+
+  
    test("fold over selector with one-direction flow")  {
-    import gopherApi._
-    for(i <- 1 to 100) {
+    
      val ch = makeChannel[Int](0)
      val quit = Promise[Boolean]()
-     val r = select.afold(0){ (x,s) =>
-               s match {
+     val quitChannel = quit.future.asChannel
+     val r = async {
+        select.fold(0){ (x,s) =>
+               s.apply{
                  case a:ch.read =>  x+a
-                 case q:Boolean if (q==quit.future.read) => CurrentFlowTermination.exit(x)
+                 case q: quitChannel.read => SelectFold.Done(x) 
                }
-             }
+        }
+     }
      ch.awriteAll(1 to 10) onComplete { _ => quit success true }
-     val sum = Await.result(r, 3 second)
-     assert(sum==(1 to 10).sum)
-    }
+     async {
+        val sum = await(r)
+        assert(sum==(1 to 10).sum)
+     }
    }
 
+   
    test("append for finite unbuffered stream") {
-      val w = new Waiter
-      val ch1 = gopherApi.makeChannel[Int](0)
-      val ch2 = gopherApi.makeChannel[Int](0)
+      val ch1 = makeChannel[Int](0)
+      val ch2 = makeChannel[Int](0)
       val appended = ch1 append ch2
       var sum = 0
       var prev = 0
       var monotonic = true
-      val f = go { for(s <- appended) {
+      val f = async { for(s <- appended) {
                      // bug in compiler 2.11.7
                      //w{assert(prev < s)}
                      //if (prev >= s) w{assert(false)}
@@ -76,13 +76,25 @@ class UnbufferedSelectSuite extends FunSuite with Waiters
       val a1 = ch1.awriteAll(1 to 10)
       val a2 = ch2.awriteAll((1 to 10)map(_*100))
       // it works, but for buffered channeld onComplete can be scheduled before. So, <= instead ==
-      a1.onComplete{ case _ => { w{assert(sum == 55)};  ch1.close(); w.dismiss() } }
-      a2.onComplete{ case _ => { w{assert(sum == 5555)}; w{assert(monotonic)}; w.dismiss() } }
-      w.await(timeout(10 seconds), dismissals(2))
-      assert(sum==5555)
-      assert(monotonic)
+
+      async {  
+        await(a1)
+        while (sum < 55) {
+          Time.sleep(50 milliseconds)
+        } 
+        assert(sum == 55)
+        
+        // after this - read from 2
+        ch1.close() 
+
+        await(a2)
+        while(sum < 5555) {
+          Time.sleep(50 milliseconds)
+        }
+        assert(sum == 5555)
+        assert(monotonic)
+      }
    }
 
-   lazy val gopherApi = CommonTestObjects.gopherApi
    
 }
