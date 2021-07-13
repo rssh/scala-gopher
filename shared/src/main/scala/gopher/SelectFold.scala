4e24cb098dd9843155733a4467c02828bf334e05
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-07-13 19:37:24 +0300

    scaladoc for Select.fold updated

--- shared/src/main/scala/gopher/SelectFold.scala
+++ shared/src/main/scala/gopher/SelectFold.scala
@@ -1,6 +1,13 @@
 package gopher
 
+/**
+ * Helper namespace for Select.Fold return value
+ * @see [Select.fold]
+ **/
 object SelectFold:
 
+  /**
+   * return value in Select.Fold which means that we should stop folding 
+   **/
   case class Done[S](s: S)
   
